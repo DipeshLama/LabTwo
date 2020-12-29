63492577c51e3e70e0e19ae07b10a07ab77ee6df
DipeshLama <dipeslama32@gmail.com>
2020-12-29 12:57:01 +0545

    Question 1

--- app/src/main/java/com/dipesh/labworktwo/MainActivity.kt
+++ app/src/main/java/com/dipesh/labworktwo/MainActivity.kt
@@ -2,10 +2,26 @@ package com.dipesh.labworktwo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tvNumber:TextView
+    private lateinit var btnIncrease:Button
+    var count:Int=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvNumber=findViewById(R.id.tvNumber)
+        btnIncrease=findViewById(R.id.btnIncrease)
+
+        tvNumber.text=count.toString()
+
+        btnIncrease.setOnClickListener {
+            count++
+            tvNumber.text=count.toString()
+
+        }
     }
 }
